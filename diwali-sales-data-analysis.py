5d2bd9e589d38714d95de112782f2f0251fbfe34
Arsal-Ansari9168 <arsalansari9168@gmail.com>
2026-09-20 15:48:37 +0530

    Add files via upload

---
+++ diwali-sales-data-analysis.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+df = pd.read_csv('Diwali Sales Data.csv', encoding= 'unicode_escape')
+
+df.shape
+df.head()
+df.info()
+df.drop(['Status', 'unnamed1'], axis=1, inplace=True)
+pd.isnull(df).sum()
+df.dropna(inplace=True)
+
+df['Amount'] = df['Amount'].astype('int')
+df['Amount'].dtypes
+
+df.columns
+df.rename(columns= {'Marital_Status':'Shaadi'})
+
+df.describe()
+df[['Age', 'Orders', 'Amount']].describe()
+
+# plotting a bar chart for Gender and it's count
+ax = sns.countplot(x = 'Gender',data = df)
+for bars in ax.containers:
+    ax.bar_label(bars)
+sales_gen = df.groupby(['Gender'],
+as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
+sns.barplot(x = 'Gender',y= 'Amount' ,data = sales_gen)
+plt.show()
+
+ax = sns.countplot(data = df, x = 'Age Group', hue = 'Gender')
+for bars in ax.containers:
+    ax.bar_label(bars)    
+# Total Amount vs Age Group
+sales_age = df.groupby(['Age Group'],
+as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
+sns.barplot(x = 'Age Group',y= 'Amount' ,data = sales_age)
+plt.show()
+
+# Total Amount vs Age Group
+sales_age = df.groupby(['Age Group'],
+as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
+sns.barplot(x = 'Age Group',y= 'Amount' ,data = sales_age)
+plt.show()
+
+# total amount/sales from top 10 states
+sales_state = df.groupby(['State'],
+as_index=False)['Amount'].sum().sort_values(by='Amount',
+ascending=False).head(10)
+sns.set(rc={'figure.figsize':(15,5)})
+sns.barplot(data = sales_state, x = 'State',y= 'Amount')
+plt.show()
+
+#Marital Status
+ax = sns.countplot(data = df, x = 'Marital_Status')
+sns.set(rc={'figure.figsize':(7,5)})
+for bars in ax.containers:
+    ax.bar_label(bars)
+sales_state = df.groupby(['Marital_Status', 'Gender'],
+as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
+sns.set(rc={'figure.figsize':(6,5)})
+sns.barplot(data = sales_state, x = 'Marital_Status',y= 'Amount',
+hue='Gender')
+plt.show()
+
+#Occupation
+sns.set(rc={'figure.figsize':(20,5)})
+ax = sns.countplot(data = df, x = 'Occupation')
+for bars in ax.containers:
+    ax.bar_label(bars)
+sales_state = df.groupby(['Occupation'],
+as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
+sns.set(rc={'figure.figsize':(20,5)})
+sns.barplot(data = sales_state, x = 'Occupation',y= 'Amount')
+plt.show()
+
+#Product Category
+sns.set(rc={'figure.figsize':(20,5)})
+ax = sns.countplot(data = df, x = 'Product_Category')
+for bars in ax.containers:
+    ax.bar_label(bars)
+sales_state = df.groupby(['Product_Category'],
+as_index=False)['Amount'].sum().sort_values(by='Amount',
+ascending=False).head(10)
+sns.set(rc={'figure.figsize':(20,5)})
+sns.barplot(data = sales_state, x = 'Product_Category',y= 'Amount')
+plt.show()
+
+sales_state = df.groupby(['Product_ID'],
+as_index=False)['Orders'].sum().sort_values(by='Orders',
+ascending=False).head(10)
+sns.set(rc={'figure.figsize':(20,5)})
+sns.barplot(data = sales_state, x = 'Product_ID',y= 'Orders')
+plt.show()
+
+# top 10 most sold products (same thing as above)
+fig1, ax1 = plt.subplots(figsize=(12,7))
+df.groupby('Product_ID')['Orders'].sum().nlargest(10).sort_values(ascending=False).plot(kind='bar')
+
+plt.show()
